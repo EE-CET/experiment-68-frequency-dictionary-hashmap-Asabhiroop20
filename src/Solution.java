faaf9e003df35fa924d099c4ed0a2e192264e724
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-10 00:18:17 +0530

    Third commit of Hashmap

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.ArrayList;     
-import java.util.Collections;  
-
+import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        String line = sc.nextLine();
-        String[] words = line.split(" ");
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
+        
+                String line = sc.nextLine().trim();
+        
+                String[] words = line.split("\\s+"); 
+        
+                TreeMap<String, Integer> map = new TreeMap<>(); 
+        
+                for (String word : words) {
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+                }
+        
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                    System.out.println(entry.getKey() + ": " + entry.getValue());
+                }
+            }
         }
-
-        ArrayList<String> keys = new ArrayList<>(map.keySet());
-        Collections.sort(keys);
-
-        for (String key : keys) {
-            System.out.println(key + ": " + map.get(key));
-        }
-    }
-}
